27ceb2c3837aba3d2298638eb24cd546b22b9e32
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2022-07-07 20:05:34 +0300

    ad

--- shop-script.js
+++ shop-script.js
@@ -1,10 +1,19 @@
-// const requestURL = 'https://raw.githubusercontent.com/HollyUndead/TestTask/main/JSON%20Data%20Base/Petrovka_horeca.json';
-const requestURL  = './JSON Data Base/Petrovka_horeca.json'
-async function igms(){
+const requestURL = 'https://raw.githubusercontent.com/HollyUndead/TestTask/main/JSON%20Data%20Base/Petrovka_horeca.json';
+
+function ById(id){return document.getElementById(id)}
+
+function byClassName(ClassName){return document.getElementsByClassName(ClassName)}
+
+async function imgs(){
     const requset = new Request(requestURL);
     const respons = await fetch(requset);
     const array = await respons.json();
     console.log(array)
+    let img = document.createElement('img')
+    img.src=array.syrupHalva.img;
+    img.style.display='block';
+    ById('plateWithThings').append(img);
 }
-igms()
-let img = document.createElement('img')
+
+imgs()
+
