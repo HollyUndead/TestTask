33eba2b4da1a0ef687d94298733943f6f8c9be1b
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2022-07-07 20:39:40 +0300

    ad

--- shop-script.js
+++ shop-script.js
@@ -3,17 +3,3 @@ const requestURL = 'https://raw.githubusercontent.com/HollyUndead/TestTask/main/
 function ById(id){return document.getElementById(id)}
 
 function byClassName(ClassName){return document.getElementsByClassName(ClassName)}
-
-async function imgs(){
-    const requset = new Request(requestURL);
-    const respons = await fetch(requset);
-    const array = await respons.json();
-    console.log(array)
-    let img = document.createElement('img')
-    img.src=array.syrupHalva.img;
-    img.style.display='block';
-    ById('plateWithThings').append(img);
-}
-
-imgs()
-
